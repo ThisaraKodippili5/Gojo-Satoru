d5c6135d8afea8dccdc0ca4c1b4b5f054b162035
ThisaraKodippili5 <117185420+ThisaraKodippili5@users.noreply.github.com>
2022-11-01 21:27:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94 0781061339']
+global.premium = ['94 0781061339']
+global.ownernomer = '94 0781061339'
+global.ownername = '🐦K.T.K mod'
+global.botname = '🐦K.T.K mod'
+global.footer = '.🐦K.T.K mod'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '-Satoru'
+global.author = '🐦Yt'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
